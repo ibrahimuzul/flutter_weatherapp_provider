026cc86d34ae1d17159c39b4213b35a877665e42
iuzul <iuzul@IBRAHIMUZUL>
2020-11-20 08:45:05 +0300

    Initial Commit

---
+++ android/app/src/main/java/com/ibrahimuzultest/flutter_weatherapp_provider/MainActivity.java
@@ -0,0 +1,6 @@
+package com.ibrahimuzultest.flutter_weatherapp_provider;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/widget/weather_app.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+import 'hava_durumu_resim.dart';
+import 'location.dart';
+import 'max_min_sicaklik.dart';
+import 'sehir_sec.dart';
+import 'son_guncelleme.dart';
+
+class WeatherApp extends StatelessWidget {
+  String kullanicininSectigiSehir = "Ankara";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Weather App"),
+        actions: <Widget>[
+          IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () async {
+                kullanicininSectigiSehir = await Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => SehirSecWidget()));
+                debugPrint("Seçilen sehir: " + kullanicininSectigiSehir);
+              }),
+        ],
+      ),
+      body: Center(
+        child: ListView(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Center(
+                  child: LocationWidget(
+                    secilenSehir: kullanicininSectigiSehir,
+                  )),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Center(child: SonGuncellemeWidget()),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Center(child: HavaDurumuResimWidget()),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Center(child: MaxveMinSicaklikWidget()),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
